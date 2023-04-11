d46e671e5c0d78cbd9b7f713703f73de8a7669b4
Ricardo Paz <ricardopdsf@gmail.com>
2023-04-11 18:18:18 -0300

    fix: change google pixel id.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -51,7 +51,7 @@ export default function App({ Component, pageProps }) {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NBMVSKQ');
+          })(window,document,'script','dataLayer','GTM-TCLTJMC');
         `}
       </Script>
 
@@ -65,7 +65,7 @@ export default function App({ Component, pageProps }) {
             width="0"
             height="0"
             style={{ display: 'none', visibility: 'hidden' }}
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NBMVSKQ"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TCLTJMC"
           />
           </noscript>
         {/* <!-- End Google Tag Manager (noscript) --> */}
